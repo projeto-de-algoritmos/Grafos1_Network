269a5afcf83b3468c410501b9c262f936f26f111
Eduardo Gurgel <51385738+EduardoGurgel@users.noreply.github.com>
2022-02-03 13:06:15 -0300

    Initial commit

---
+++ TowerOfHanoi/TowerOfHanoi/Nodes/BaseNode/BaseNode.swift
@@ -0,0 +1,21 @@
+//
+//  BaseNode.swift
+//  TowerOfHanoi
+//
+//  Created by Luis Gustavo Avelino de Lima Jacinto on 02/11/19.
+//  Copyright © 2019 Luis Gustavo Avelino de Lima Jacinto. All rights reserved.
+//
+
+import SpriteKit
+
+class BaseNode: SKSpriteNode {
+
+    init() {
+        let texture = SKTexture(imageNamed: "base")
+        super.init(texture: texture, color: .clear, size: texture.size())
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
